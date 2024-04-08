7e49c666fc6b3ef441082fbfe2c0f374e1dbb2ae
jrr997 <zackchangjx@foxmail.com>
2024-04-08 10:24:58 +0800

    fix: config error

--- src/handleFile.ts
+++ src/handleFile.ts
@@ -4,7 +4,10 @@ const path = require('path');
 const fs = require('fs');
 const ejs = require('ejs');
 
-export const handleFile = (uri: vscode.Uri, { ext, fileName, templateDir, templateConfig }: IExtraInfo) => {
+export const handleFile = (
+  uri: vscode.Uri,
+  { ext, fileName, templateDir, templateConfig }: IExtraInfo
+) => {
   const normalizedPath = path.normalize(uri.fsPath);
   const tplManager = new TemplateManager(templateConfig, templateDir);
   const tplPath = tplManager.findTemplate(normalizedPath!);
@@ -16,24 +19,36 @@ export const handleFile = (uri: vscode.Uri, { ext, fileName, templateDir, templa
   const content = fs.readFileSync(tplPath, 'utf-8');
   const edit = new vscode.WorkspaceEdit();
   const paths = normalizedPath.split(path.sep);
-  edit.insert(uri, new vscode.Position(0, 0), ejs.render(content, { paths, fileName, ext, normalizedPath, FileName: fileName!.charAt(0).toUpperCase() + fileName.slice(1)}));
+  edit.insert(
+    uri,
+    new vscode.Position(0, 0),
+    ejs.render(content, {
+      paths,
+      fileName,
+      ext,
+      normalizedPath,
+      FileName: fileName!.charAt(0).toUpperCase() + fileName.slice(1),
+    })
+  );
 
   vscode.workspace.applyEdit(edit);
 };
 
-
-
 class TemplateManager {
   private internalTemplates: ITemplates = {
-    ['tsx.tpl']: ["\.tsx$"],
-    ['jsx.tpl']: ["\.jsx$"],
-    ['js.tpl']: ["\.[(js)(ts)]$"],
+    ['tsx.tpl']: ['(.tsx)$'],
+    ['jsx.tpl']: ['(.jsx)$'],
+    ['js.tpl']: ['(.[(js)(ts)])$'],
   };
   private userTemplates: ITemplates = {};
   private templateDir: string = '';
 
-  private _findTemplate(normalizedPath: string, templates: ITemplates, templateDir: string) {
-    for (const [key, value] of Object.entries(templates)) {   
+  private _findTemplate(
+    normalizedPath: string,
+    templates: ITemplates,
+    templateDir: string
+  ) {
+    for (const [key, value] of Object.entries(templates)) {
       for (const pattern of value) {
         const reg = new RegExp(pattern as string);
         if (reg.test(normalizedPath)) {
@@ -49,8 +64,18 @@ class TemplateManager {
     this.templateDir = templateDir ?? '';
   }
 
-  public findTemplate(normalizedPath: string ) {
-    return this._findTemplate(normalizedPath, this.userTemplates, this.templateDir) || this._findTemplate(normalizedPath, this.internalTemplates, path.join(__dirname, 'template'));
+  public findTemplate(normalizedPath: string) {
+    return (
+      this._findTemplate(
+        normalizedPath,
+        this.userTemplates,
+        this.templateDir
+      ) ||
+      this._findTemplate(
+        normalizedPath,
+        this.internalTemplates,
+        path.join(__dirname, 'template')
+      )
+    );
   }
 }
-
